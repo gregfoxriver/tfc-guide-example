d8df0ba567143c5c1086f5a552840ac1ae14a4a9
gregfoxriver <greg@foxriverinfosec.com>
2021-03-30 17:55:06 -0500

    Updated Main to fix errors

--- main.tf
+++ main.tf
@@ -19,10 +19,12 @@ resource "random_pet" "table_name" {}
 resource "azurerm_cosmosdb_account" "tfc_example_account" {
   name                 = "TFCGuideExampleAccount"
   resource_group_name  = azurerm_resource_group.rg.name
+  location = var.location
+  offer_type = "Standard"
 }
 
 resource "azurerm_cosmosdb_table" "tfc_example_table" {
   name                = "${var.db_table_name}-${random_pet.table_name.id}"
-  resource_group_name = data.azurerm_cosmosdb_account.tfc_example_account.resource_group_name
-  account_name        = data.azurerm_cosmosdb_account.tfc_example_account.name
+  resource_group_name = azurerm_resource_group.rg.name
+  account_name        = azurerm_cosmosdb_account.tfc_example_account.name
 }
--- versions.tf
+++ versions.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.46.0"
+      version = "2.53.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = ">=3.0.0"
+      version = "3.1.0"
     }
   }
 }
